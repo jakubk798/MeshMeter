8eae759dbbb32f3f8dea14842d49175f357f2f5b
Kuba (PC) <kakubus@outlook.com>
2026-05-04 21:25:28 +0200

    Pinout with ESP32-C6

---
+++ code/wMbus-lib-1.4.7/utils.hpp
@@ -0,0 +1,22 @@
+/***********************************************************************************
+    Filename: utils.hpp
+***********************************************************************************/
+
+#ifndef _UTILS_H
+#define _UTILS_H
+
+#include <Arduino.h>
+#include <stdint.h>
+#include <vector>
+
+//----------------------------------------------------------------------------------
+//  Function Declareration
+//----------------------------------------------------------------------------------
+void dumpHex(uint8_t* data, uint8_t len, bool newLine = true);
+unsigned char *safeButUnsafeVectorPtr(std::vector<unsigned char> &v);
+bool decrypt_TPL_AES_CBC_IV(std::vector<unsigned char> &frame, std::vector<unsigned char>::iterator &pos,
+                            std::vector<unsigned char> &key, unsigned char *iv,
+                            int *num_encrypted_bytes,
+                            int *num_not_encrypted_at_end);
+
+#endif
